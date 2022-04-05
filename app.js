179470ceb7e784ea5bb8c0ea0cd7d5a227042ac4
Jared <jkpeople1856@gmail.com>
2022-04-05 09:51:35 -0500

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-console.log("Hello World!\n==========\n");
+// Giphy Fetch API by Search Term
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const GIPHY_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIPHY_KEY = "";
+
+let inputEle = document.querySelector("#gifSearch");
+let searchBtnEle = document.querySelector("#searchBTN");
+let imgEle = document.querySelector("#giphy");
+let titleEle = document.querySelector("#gifTitle");
+
+searchBtnEle.addEventListener("click", searchGif);
+
+async function searchGif() {
+    let searchTerm = inputEle.value;
+
+    try {
+        let response = await fetch(
+            (`${GIPHY_URL}?api_key=${GIPHY_KEY}&s=${searchTerm})`
+        );
+        let responseBody = await response.json();
+        console.log(responseBody);
+        titleEle.textContent - responseBody.data.title;
+        imgEle.src = responseBody.data.images.original.url;
+    } catch (error) {
+        console.error(error);
+    }
+}
